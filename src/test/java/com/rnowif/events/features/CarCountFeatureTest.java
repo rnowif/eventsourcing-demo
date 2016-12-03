5e709b5a2dd36fc1c9bff60803daa5fa0f351b48
Renaud Humbert-Labeaumaz <rnow.if@gmail.com>
2016-12-03 23:28:14 +0100

    test(*): Add PBT to feature test

--- src/test/java/com/rnowif/events/features/CarCountFeatureTest.java
+++ src/test/java/com/rnowif/events/features/CarCountFeatureTest.java
@@ -1,34 +1,43 @@
 package com.rnowif.events.features;
 
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import com.rnowif.events.domain.parkinglot.CarCountHandler;
 import com.rnowif.events.domain.parkinglot.ParkingLot;
 import com.rnowif.events.domain.parkinglot.events.CarEntered;
 import com.rnowif.events.domain.parkinglot.events.CarExited;
 import com.rnowif.events.infra.SimpleEventBus;
-import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeTrue;
 
+@RunWith(JUnitQuickcheck.class)
 public class CarCountFeatureTest {
     
-    @Test
-    public void should_count_cars_when_entering_and_exiting() {
+    @Property
+    public void should_count_cars_when_entering_and_exiting(
+            @InRange(min = "0", max = "100") int nbCarsIn,
+            @InRange(min = "0", max = "100") int nbCarsOut
+    ) {
+        assumeTrue(nbCarsIn >= nbCarsOut);
         SimpleEventBus eventBus = new SimpleEventBus();
         CarCountHandler handler = new CarCountHandler();
         eventBus.register(CarEntered.class, handler::apply);
         eventBus.register(CarExited.class, handler::apply);
         ParkingLot parkingLot = new ParkingLot(eventBus);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < nbCarsIn; i++) {
             parkingLot.enterCar();
         }
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < nbCarsOut; i++) {
             parkingLot.exitCar();
         }
 
-        assertThat(handler.getNbCars(), is(3));
+        assertThat(handler.getNbCars(), is(nbCarsIn - nbCarsOut));
 
     }
 }
